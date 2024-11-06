ac09d2d027d1b1a7bdeb0c5f9efe6377043ad069
Falk33n <tim.falk00@gmail.com>
2024-11-06 13:50:50 +0100

    changed config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/inlamningsuppgift-tim-falk-js2',
+};
 
 export default nextConfig;
